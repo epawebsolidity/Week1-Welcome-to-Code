5f00fe4851beba68adc5893ecd39c034ccb25387
Epa Fe <epaweb3js@gmail.com>
2026-01-30 11:29:34 +0700

    quiz 2, saol 1 and 2 done

---
+++ quiz/part2/soal2.js
@@ -0,0 +1,40 @@
+// Problem
+// Pada tugas ini kamu diminta untuk "memecah" sebuah kalimat dan menampilkan setiap kata didalamnya.
+//  Untuk soal nomor ini, gunakan akses satu per satu karakter dari string untuk mengambil setiap huruf dalam kata.
+//   Terasa manual? Tidak apa-apa, kita coba ini dulu untuk saat ini.
+
+// Hints
+// Saat kamu mendapatkan tiap huruf, untuk membentuk setiap kata kamu tinggal menggunakan simbol + untuk membentuk kata
+//  tersebut!
+
+
+/*
+/////////////////////////////////////////
+            ASSIGN NILAI VARIABEL
+///////////////////////////////////////
+*/
+
+let word = 'wow JavaScript is so cool';
+let exampleFirstWord = word[0] + word[1] + word[2];
+let exampleFirstWordTwo = word[4] + word[5] + word[6] + word[7] + word[8] + word[9] + word[10] + word[11] + word[12] + word[13];
+let exampleFirstWordTree = word[15] + word[16];
+let exampleFirstWordFour = word[18] + word[19];
+let exampleFirstWordFive = word[21] + word[22] + word[23] + word[24];
+
+/*
+/////////////////////////////////////////
+            CONSOLE OR LOG
+///////////////////////////////////////
+*/
+
+console.log('First Word: ' + exampleFirstWord);
+console.log('Second Word: ' + exampleFirstWordTwo);
+console.log('Third Word: ' + exampleFirstWordTree);
+console.log('Fourth Word: ' + exampleFirstWordFour);
+console.log('Fifth Word: ' + exampleFirstWordFive);
+
+/*
+/////////////////////////////////////////
+            END OF CODE
+///////////////////////////////////////
+*/
